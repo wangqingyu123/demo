c4ad6f1ed852e7018358a03c845718b8c9e20398
zhuchao <zc520yzy@126.com>
2017-02-10 09:54:54 +0800

    test

--- onethink/newfile.php
+++ onethink/newfile.php
@@ -1,6 +1,27 @@
-
 <?php 
 
-phpinfo();
+class test{
+	
+	const MIN_VALUE = 0.0;
+	
+	
+	function __construct(){
+		
+		$redis = new redis();
+		
+		//range(1,10) = self::BUG;
+				
+		var_dump(self::MIN_VALUE);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
 
-?>
+$t =new test();
+
+
